9011d7e218be6cb85952908f459fb6255e8a1b06
Gabriel Alves <gabriel.asouza2608@hotmail.com>
2023-08-16 11:37:08 -0300

    fix: fixing routing

--- src/Routes.ts
+++ src/Routes.ts
@@ -1,15 +1,16 @@
 import { Router } from "express";
-import UsersController from "./controller/UsersController";
 import { userAuth } from "./middlewares/userAuth";
+
+import UsersController from "./controller/UsersController";
 import TradesController from "./controller/TradesController";
 
 const router: Router = Router()
 
-router.post('/user/signup', UsersController.signup);
-router.post('/user/signin', UsersController.signin);
+router.post('/auth/signup', UsersController.signup);
+router.post('/auth/signin', UsersController.signin);
 router.get('/users', UsersController.findAll);
 
-router.get('/user/:id', userAuth, UsersController.show);
+router.get('/user/me', userAuth, UsersController.show);
 router.put('/user/:id', userAuth, UsersController.update);
 router.delete('/user/:id', userAuth, UsersController.delete);
 
