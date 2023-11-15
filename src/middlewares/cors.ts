6179af57e84577c5b2393faa587f013334c5ae01
Gabriel Souza <gabriel.asouza2608@hotmail.com>
2023-11-15 20:24:24 -0300

    fix: allor cors

--- src/middlewares/cors.ts
+++ src/middlewares/cors.ts
@@ -1,7 +1,7 @@
 import { NextFunction, Request, Response } from "express";
 
 export default function Cors(request: Request, response: Response, next: NextFunction) {
-  response.setHeader('Access-Control-Allow-Origin', 'http://localhost:5173');
+  response.setHeader('Access-Control-Allow-Origin', 'https://currency-exchange-app-production.up.railway.app');
   response.setHeader('Access-Control-Allow-Methods', '*');
   response.setHeader('Access-Control-Allow-Headers', '*');
 
