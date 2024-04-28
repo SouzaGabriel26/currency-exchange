fcf4022a22ba03d8c02b18833a1c50b9eefb369d
Gabriel Souza <gabriel.asouza2608@hotmail.com>
2024-04-28 16:31:05 -0300

    chore: Add docs about how to run

--- api/src/middlewares/cors.ts
+++ api/src/middlewares/cors.ts
@@ -1,7 +1,10 @@
 import { NextFunction, Request, Response } from "express";
 
+const isProduction = process.env.NODE_ENV === 'production';
+const allowedOrigin = process.env.ALLOWED_ORIGIN!;
+
 export default function Cors(request: Request, response: Response, next: NextFunction) {
-  response.setHeader('Access-Control-Allow-Origin', 'https://currency-exchange-app-production.up.railway.app');
+  response.setHeader('Access-Control-Allow-Origin', isProduction ? allowedOrigin : '*');
   response.setHeader('Access-Control-Allow-Methods', '*');
   response.setHeader('Access-Control-Allow-Headers', '*');
 
